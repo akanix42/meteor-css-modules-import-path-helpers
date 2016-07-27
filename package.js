ce37969e8aab944965131dc6803a787d9dd38b1d
Nathan Reid <nathan-code@nathantreid.com>
2016-07-26 23:28:55 -0500

    update version information for 0.1.4 release

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'nathantreid:css-modules-import-path-helpers',
-	version: '0.1.3',
+	version: '0.1.4',
 	// Brief, one-line summary of the package.
 	summary: 'Helper Library for package file paths',
 	// URL to the Git repository containing the source code for this package.
